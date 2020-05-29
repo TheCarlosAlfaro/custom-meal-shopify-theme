fed3239731dc458f3e63fdcfb4c7c4622acab4f8
devbysalas <rgkalin@gmail.com>
2020-05-29 16:56:44 -0700

    Adds JS to identified which options have been made

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -1,8 +1,27 @@
 console.log('My custom JS is working');
 
-// const cartCount = document.querySelector('[role="status"]');
+const sideOptions = {
+  carbohydrates: {
+    option1: {
+      protein: 1,
+      carbs: 11.2,
+      fat: 0,
+      calories: 51,
+    },
+  },
+  vegetables: {},
+};
 
-// cartCount.addEventListener('change', function (e) {
-//   console.log(e);
-// });
-// console.log(cartCount);
+const selectCarbElement = document.querySelector('#carbohydrates');
+
+selectCarbElement.addEventListener('change', (event) => {
+  const result = document.querySelector('#carb-details');
+  if (event.target.value === '') {
+    result.textContent = `Choose one option`;
+  } else {
+    result.textContent = `You like ${event.target.tagName}`;
+  }
+  console.log(
+    event.target.options[event.target.options.selectedIndex].dataset.option
+  );
+});
