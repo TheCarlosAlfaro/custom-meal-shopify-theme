7154b99cadd26d2af3b1078d89a9ffd7db082088
devbysalas <rgkalin@gmail.com>
2020-05-29 18:09:20 -0700

    Adds dynamic info on property selection

--- assets/custom-js.js
+++ assets/custom-js.js
@@ -2,7 +2,7 @@ console.log('My custom JS is working');
 
 const sideOptions = {
   carbohydrates: {
-    option1: {
+    jazmineRice: {
       protein: 1,
       carbs: 11.2,
       fat: 0,
@@ -19,7 +19,20 @@ selectCarbElement.addEventListener('change', (event) => {
   if (event.target.value === '') {
     result.textContent = `Choose one option`;
   } else {
-    result.textContent = `You like ${event.target.tagName}`;
+    result.innerHTML = `<span>${event.target.value}</span>
+    <div>
+    Protein: ${sideOptions.carbohydrates.jazmineRice.protein}
+    </div>
+    <div>
+    Carbs: ${sideOptions.carbohydrates.jazmineRice.carbs}
+    </div>
+    <div>
+    Fat: ${sideOptions.carbohydrates.jazmineRice.fat}
+    </div>
+    <div>
+    Calories: ${sideOptions.carbohydrates.jazmineRice.calories}
+    </div>
+    `;
   }
   console.log(
     event.target.options[event.target.options.selectedIndex].dataset.option
