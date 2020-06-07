87ed08b7773c09685caa35581a98a11469bf0a5d
devbysalas <rgkalin@gmail.com>
2020-06-06 22:19:50 -0700

    Adds code to make user to choose a variant, also moves custom.js inside actual product template

--- assets/theme.js
+++ assets/theme.js
@@ -7564,3 +7564,19 @@ function onYouTubeIframeAPIReady() {
 }
 
 $(theme.init);
+
+$(document).ready(function () {
+  if (typeof productOptions != 'undefined') {
+    for (i = 0; i < productOptions.length; i++) {
+      $('.single-option-selector:eq(' + i + ')')
+        .filter(function () {
+          return $(this).find('option').length > 1;
+        })
+        .prepend(
+          '<option value="">Select ' + productOptions[i][i] + '</option>'
+        )
+        .val('')
+        .trigger('change');
+    }
+  }
+});
